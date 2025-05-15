81d489dbc5f84fe70b324bd4476865e56e80f834
Nikhil Kesarwani <bmv.nikhil@gmail.com>
2025-05-15 12:39:34 +0530

    "Initial Commit of DataTypes_Programs"

---
+++ datatypes/NonPrimitive_DataTypes.java
@@ -0,0 +1,19 @@
+/* Non Primitive_DataTypes:-Non_Primitive DataTypes are those datatypes which are derived from the primitive ddatypes
+KeyPoints:The size of the non_primitive_datatypes is not fixed.
+eg:-Arrays,Strings,etc
+ */
+package datatypes;
+public class NonPrimitive_DataTypes
+{
+    public static void main(String[] args)
+    {
+        //1.Arrays:used to store values of same type
+        int[]numbers={10,20,30};
+        System.out.println("First_Number: "+numbers[0]);
+        System.out.println("Second_Number: "+numbers[1]);
+        System.out.println("Third_Number: "+numbers[2]);
+
+        //2.Strings:used to store text
+        String name="Nikhil";
+    }
+}
---
+++ datatypes/Primitive_DataTypes.java
@@ -0,0 +1,30 @@
+/*
+Write a program to understand the concept of primitive_datatypes
+Primitive_DataTypes:Primitive Data Types are those datatypes which are predefined in the complier
+KeyPoints:-The size of the primitive_datatypes is always fixed..
+eg:-int,byte,short,long,etc
+*/
+package datatypes;
+public class Primitive_DataTypes
+{
+    public static void main(String[] args)
+    {
+        byte b=10;
+        short s=2000;
+        int i=10000;
+        long l=10000000L;
+        float f=10.2f;
+        double d=19.99;
+        char c='A';
+        boolean bool=true;
+
+        System.out.println("byte:" +b);
+        System.out.println("short:" +s);
+        System.out.println("integer:" +i);
+        System.out.println("long:" +l);
+        System.out.println("float:" +f);
+        System.out.println("char:" +c);
+        System.out.println("boolean:" +bool);
+    }
+
+}
