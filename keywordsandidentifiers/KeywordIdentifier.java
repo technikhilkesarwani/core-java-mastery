816ae81633fb7286ab3a4b3b600dec71e51f07c8
Nikhil Kesarwani <bmv.nikhil@gmail.com>
2025-05-16 09:51:21 +0530

    "Initial Commit of KeywordsAndIdentifiers Programs"

---
+++ keywordsandidentifiers/KeywordIdentifier.java
@@ -0,0 +1,26 @@
+/* Write a program to understand the concept of keywords and identifiers
+Keywords:Keywords are the pre-defined reserved keywords that a java compiler can easily understand.
+Points:Java Keywords cannot be used as identifiers
+
+Identifiers:Identifiers are the name given to either class,constructor ,method etc.
+*  */
+package keywordsandidentifiers;
+public class KeywordIdentifier
+{
+    //'int' is a keyword and 'age' is an identifier
+    int age=18;
+
+    //'public' and 'void' are keywords 'showDetails' is an identifier
+    public void showDetails()
+    {
+        //'System','out' and 'println' are identifiers
+        System.out.println("Age is "+age);
+    }
+    //'main' 'String' ,'args' are identifiers,'static and 'public' are keywords
+    public static void main(String[] args)
+    {
+        //'new' is a keyword ,'obj' is an identifier
+        KeywordIdentifier obj=new KeywordIdentifier();
+        obj.showDetails();
+    }
+}
